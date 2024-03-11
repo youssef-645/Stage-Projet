b72aa3899acb13347191dce62ef586156008ac65
userdev34 <tom.tanger23@gmail.com>
2024-03-11 17:56:47 +0000

    ajoute migrations/controlleur/model eleves

---
+++ database/migrations/2024_03_11_175413_create_eleves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('eleves', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('date_naissance');
+            $table->string('sexe');
+            $table->string('adresse');
+            $table->string('ville');
+            $table->string('code_postal');
+            $table->string('email')->unique();
+            $table->string('telephone');
+            $table->string('classe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('eleves');
+    }
+};
