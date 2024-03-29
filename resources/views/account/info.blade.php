46fb7d0d1515026229a0470fba061d61c7e32a3d
userdev34 <tom.tanger23@gmail.com>
2024-03-29 08:34:13 +0000

    finit implemetation de update password.

--- resources/views/account/info.blade.php
+++ resources/views/account/info.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 
-@section('title', 'user Information')
+@section('title', 'Informations de l\'utilisateur')
 
 @section('content')
 <div class="container">
-  <h1 class="mb-4">Account Informations</h1>
+  <h1 class="mb-4">Informations du compte</h1>
   <table class="table table-bordered">
     <tbody>
+      <!-- Informations de l'utilisateur -->
       <tr>
         <th>ID</th>
         <td>{{ $user->id }}</td>
       </tr>
       <tr>
-        <th>Username</th>
+        <th>Nom d'utilisateur</th>
         <td>{{ $user->username }}</td>
       </tr>
       <tr>
@@ -49,5 +50,68 @@
       </tr>
     </tbody>
   </table>
+
+  <!-- Formulaire de modification de mot de passe -->
+  <div class="card">
+    <div class="card-header">{{ __('Update Password') }}</div>
+
+    <div class="card-body">
+      @if(session('error'))
+      <div class="alert alert-danger">{{ session('error') }}</div>
+      @endif
+
+      @if(session('success'))
+      <div class="alert alert-success">{{ session('success') }}</div>
+      @endif
+
+      <form method="POST" class="mb-4" action="{{ route('account.password') }}">
+        @csrf
+
+        <div class="form-group row">
+          <label for="old_password" class="col-md-4 col-form-label text-md-right">{{ __('Old Password') }}</label>
+
+          <div class="col-md-6">
+            <input id="old_password" type="password" class="form-control my-2 @error('old_password') is-invalid @enderror" name="old_password" required autocomplete="current-password">
+
+            @error('old_password')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
+
+          <div class="col-md-6">
+            <input id="password" type="password" class="form-control my-2 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+            @error('password')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="password_confirmation" class="col-md-4 col-form-label text-md-right">{{ __('Confirm New Password') }}</label>
+
+          <div class="col-md-6">
+            <input id="password_confirmation" type="password" class="form-control my-2" name="password_confirmation" required autocomplete="new-password">
+          </div>
+        </div>
+
+        <div class="form-group row mb-0">
+          <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary">
+              {{ __('Update Password') }}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
 @endsection
