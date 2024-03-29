8551278b097e5a1aff8fd56222424ea364639f47
userdev34 <tom.tanger23@gmail.com>
2024-03-29 08:05:24 +0000

    ajoute redirection card click

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,16 +6,11 @@
 <!-- Dashboard -->
 <div class="row p-4 g-4">
     <div class="col-md-6">
-        <div class="card sky-background" style="height: 150px;background-color:tomato">
+        <div class="card sky-background clickable-card" data-route="{{ route('classes.index') }}" style="height: 150px; background-color: tomato;">
             <div class="card-body d-flex align-items-center justify-content-center">
                 <i class="fa fa-home fa-5x text-white"></i>
                 <div class="ml-3 text-white">
                     Les Classes
-                    <span>
-                        <form action="{{ route('classes.index') }}" method="GET">
-                            <button type="submit" class="btn btn-link btn-sm">Details</button>
-                        </form>
-                    </span>
                 </div>
             </div>
         </div>
@@ -23,16 +18,11 @@
 
     <!-- Enseignants -->
     <div class="col-md-6">
-        <div class="card sky-background" style="height: 150px;;background-color:lightgreen">
+        <div class="card sky-background clickable-card" data-route="{{ route('enseignants.index') }}" style="height: 150px; background-color: lightgreen;">
             <div class="card-body d-flex align-items-center justify-content-center">
                 <i class="fa fa-users fa-5x text-white"></i>
                 <div class="ml-3 text-white">
                     Les Enseignants
-                    <span>
-                        <form action="{{ route('enseignants.index') }}" method="GET">
-                            <button type="submit" class="btn btn-link btn-sm">Details</button>
-                        </form>
-                    </span>
                 </div>
             </div>
         </div>
@@ -40,16 +30,11 @@
 
     <!-- Matieres -->
     <div class="col-md-6">
-        <div class="card sky-background" style="height: 150px;;background-color:grey">
+        <div class="card sky-background clickable-card" data-route="{{ route('matieres.index') }}" style="height: 150px; background-color: grey;">
             <div class="card-body d-flex align-items-center justify-content-center">
                 <i class="fa fa-book fa-5x text-white"></i>
                 <div class="ml-3 text-white">
                     Les Matieres
-                    <span>
-                        <form action="{{ route('matieres.index') }}" method="GET">
-                            <button type="submit" class="btn btn-link btn-sm">Details</button>
-                        </form>
-                    </span>
                 </div>
             </div>
         </div>
@@ -57,19 +42,28 @@
 
     <!-- Notes -->
     <div class="col-md-6">
-        <div class="card sky-background" style="height: 150px;;background-color:orange">
+        <div class="card sky-background clickable-card" data-route="{{ route('notes.index') }}" style="height: 150px; background-color: orange;">
             <div class="card-body d-flex align-items-center justify-content-center">
                 <i class="fa fa-clipboard fa-5x text-white"></i>
                 <div class="ml-3 text-white">
                     Les Notes
-                    <span>
-                        <form action="{{ route('notes.index') }}" method="GET">
-                            <button type="submit" class="btn btn-link btn-sm">Details</button>
-                        </form>
-                    </span>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var clickableCards = document.querySelectorAll('.clickable-card');
+
+        clickableCards.forEach(function(card) {
+            card.addEventListener('click', function() {
+                var route = this.getAttribute('data-route');
+                window.location.href = route;
+            });
+        });
+    });
+</script>
+
 @endsection
