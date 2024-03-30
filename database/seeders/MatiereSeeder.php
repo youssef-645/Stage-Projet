b7294c2be031e0d9cf2f72e137ab671facb67a35
userdev34 <tom.tanger23@gmail.com>
2024-03-30 09:47:52 +0000

    update seeders pour les relations matiere enseignant classe.

--- database/seeders/MatiereSeeder.php
+++ database/seeders/MatiereSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Classe;
+use App\Models\Enseignant;
+use App\Models\Groupe;
 use App\Models\Matiere;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,10 +19,19 @@ class MatiereSeeder extends Seeder
     {
         $faker = Faker::create();
 
+        // Récupérer tous les enseignants, groupes et classes existants
+        $enseignants = Enseignant::all();
+        $groupes = Groupe::all();
+        $classes = Classe::all();
+
         for ($i = 0; $i < 50; $i++) {
-            Matiere::create([
+            $matiere = Matiere::create([
                 'nom' => $faker->word,
             ]);
+
+            $matiere->enseignantes()->attach($enseignants->random(rand(1, 5))->pluck('id'));
+            $matiere->groupes()->attach($groupes->random(rand(1, 3))->pluck('id'));
+            $matiere->classes()->attach($classes->random(rand(1, 3))->pluck('id'));
         }
     }
 }
