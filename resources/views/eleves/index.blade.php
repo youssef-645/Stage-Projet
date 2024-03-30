da4251f5fccb88c6d4ebad98b1a021a19fd16e17
userdev34 <tom.tanger23@gmail.com>
2024-03-30 09:20:34 +0000

    ajoute logout a sidebar.

--- resources/views/eleves/index.blade.php
+++ resources/views/eleves/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h2 class="my-4">List des Eleves</h2>
-    <form action="{{ route('eleves.search') }}" method="GET">
+    <form action="{{ route('eleves.search') }}" method="POST">
         <div class="form-group">
             <input type="text" name="query" class="form-control w-75" placeholder="Search...">
             <button type="submit" class="mt-4 mb-4 btn btn-primary">Search</button>
