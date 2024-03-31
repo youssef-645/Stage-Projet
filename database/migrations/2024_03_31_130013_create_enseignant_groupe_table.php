aa95a2624456310924b218b44d79252743c6a66a
userdev34 <tom.tanger23@gmail.com>
2024-03-31 13:01:39 +0000

    ajoute table de relation enseignant_groupe

---
+++ database/migrations/2024_03_31_130013_create_enseignant_groupe_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enseignant_groupe', function (Blueprint $table) {
+            $table->foreignId('enseignant_id')->constrained()->onDelete('cascade');
+            $table->foreignId('groupe_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enseignant_groupe');
+    }
+};
